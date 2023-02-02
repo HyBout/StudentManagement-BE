0bba4a6f685b7890181e99755279ec52569d054b
Bao Huy <pbhuy0909@gmail.com>
2023-02-02 23:33:10 +0700

    restarting server

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const fs = require('fs')
 const cors = require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 
 
 //config
@@ -12,7 +18,7 @@ let routesPath = './api/routes/'
 let app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors());
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 fs.readdirSync(routesPath).forEach(E => {
   try {
