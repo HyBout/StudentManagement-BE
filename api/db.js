10034f03469d36b29b3cc44517b6bdf155e9c40c
Bao Huy <pbhuy0909@gmail.com>
2023-02-02 16:47:53 +0700

    restart server

--- api/db.js
+++ api/db.js
@@ -1,15 +1,6 @@
 const initOptions = {}
 const pgp = require('pg-promise')(initOptions)
 
-const cn = (
-  {
-    host: process.env.db_host,
-    user: process.env.db_user,
-    port: process.env.db_port,
-    password: process.env.db_password,
-    database: process.env.db_database,
-    max: 30
-  }
-);
+const cn = 'postgresql://postgres:@Phambaohuy0909@db.suxwmcaimdvmhbabdnte.supabase.co:5432/postgres'
 
 module.exports = pgp(cn)
